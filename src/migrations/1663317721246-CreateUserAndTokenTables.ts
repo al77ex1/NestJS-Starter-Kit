64b1cecb58e780f6631bed18d6a67ef3e72db862
Alex Shatunov <al77ex1@gmail.com>
2022-09-26 20:34:09 +0300

    added graphql packages

--- src/migrations/1663317721246-CreateUserAndTokenTables.ts
+++ src/migrations/1663317721246-CreateUserAndTokenTables.ts
@@ -1,18 +1,27 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class CreateUserAndTokenTables1663317721246 implements MigrationInterface {
-    name = 'CreateUserAndTokenTables1663317721246'
+export class CreateUserAndTokenTables1663317721246
+    implements MigrationInterface
+{
+    name = 'CreateUserAndTokenTables1663317721246';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(50) NOT NULL, "email" character varying(50) NOT NULL, "password" character varying(255) NOT NULL, "role" "public"."users_role_enum" NOT NULL DEFAULT 'user', "isEmailVerified" boolean NOT NULL DEFAULT false, "createdAt" date DEFAULT NOW(), "updatedAt" date DEFAULT NOW(), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "auth" ("id" SERIAL NOT NULL, "token" character varying(255) NOT NULL, "type" character varying(50) NOT NULL, "expires" TIMESTAMP WITH TIME ZONE NOT NULL, "blacklisted" boolean NOT NULL DEFAULT false, "userId" uuid, CONSTRAINT "PK_7e416cf6172bc5aec04244f6459" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "auth" ADD CONSTRAINT "FK_373ead146f110f04dad60848154" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(
+            `CREATE TABLE "users" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying(50) NOT NULL, "email" character varying(50) NOT NULL, "password" character varying(255) NOT NULL, "role" character varying(50) NOT NULL DEFAULT 'user', "isEmailVerified" boolean NOT NULL DEFAULT false, "createdAt" date DEFAULT NOW(), "updatedAt" date DEFAULT NOW(), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
+        );
+        await queryRunner.query(
+            `CREATE TABLE "auth" ("id" SERIAL NOT NULL, "token" character varying(255) NOT NULL, "type" character varying(50) NOT NULL, "expires" TIMESTAMP WITH TIME ZONE NOT NULL, "blacklisted" boolean NOT NULL DEFAULT false, "userId" uuid, CONSTRAINT "PK_7e416cf6172bc5aec04244f6459" PRIMARY KEY ("id"))`,
+        );
+        await queryRunner.query(
+            `ALTER TABLE "auth" ADD CONSTRAINT "FK_373ead146f110f04dad60848154" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "auth" DROP CONSTRAINT "FK_373ead146f110f04dad60848154"`);
+        await queryRunner.query(
+            `ALTER TABLE "auth" DROP CONSTRAINT "FK_373ead146f110f04dad60848154"`,
+        );
         await queryRunner.query(`DROP TABLE "auth"`);
         await queryRunner.query(`DROP TABLE "users"`);
     }
-
 }
